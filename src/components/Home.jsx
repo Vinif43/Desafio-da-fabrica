e82fb6395097aa9cd53e4bc4bf5596ad71e584ac
vinif <vinifmarinhoo@gmail.com>
2022-09-17 22:21:30 -0300

    card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     return(
         <>
         <Navbar />
-        <div className="card__home">
+        <div className="card__home card">
             <h1 className="titulo__home">Chester Zoo</h1>
             <p>We at Chester Zoo are a HUGE team of conservationists, scientists, educators, veterinary experts, botanists, wildlife managers, animal behaviourists, endocrinologists and environmental policy influencers! TOGETHER we are FIGHTING wildlife extinction. We are a major wildlife charity and we are making a vital contribution to tackling the global extinction crisis at a time when it’s needed most! Our zoo is vital to the future of wildlife on this planet. </p>
         </div>
